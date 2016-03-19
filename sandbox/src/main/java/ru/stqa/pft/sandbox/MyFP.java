7d434e5076a9822bb7fc2de3a1be524db18720b9
Vil-akhm <qwqw19441@yandex.ru>
2016-03-19 21:37:13 +0400

    Создан пакет для первой программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
@@ -1,7 +1,9 @@
-public class MyFP { 
-   
+package ru.stqa.pft.sandbox;
+
+public class MyFP {
+
    public static void main(String[] args) {
      System.out.println("Hello, world");
-     }
+   }
 
 }
