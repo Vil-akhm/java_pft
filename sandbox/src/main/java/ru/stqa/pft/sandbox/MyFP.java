50c0a0b2e36f7e55d59433f9c05ff73c9d0d9dc2
Vil-akhm <qwqw19441@yandex.ru>
2016-03-19 23:44:10 +0400

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
@@ -3,7 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFP {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+       hello();
+   //     hello("user");
+
+        double len =5;
+        System.out.println(area(len));
+
+        double a= 3;
+        double b =4;
+        System.out.println(area(a,b));
     }
 
+ public static void hello() {
+     System.out.println("Hello");
+ }
+
+
+    public static double area( double l){
+        return l*l;
+    }
+
+    public static double area (double a,double b){
+        return a*b;
+    }
 }
