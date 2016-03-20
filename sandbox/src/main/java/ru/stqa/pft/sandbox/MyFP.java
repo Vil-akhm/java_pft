4641c05bc6ee68cf94100b19e9ad1d3599b4084c
Vil-akhm <qwqw19441@yandex.ru>
2016-03-20 12:25:28 +0400

    методы атрибуты в объектах

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFP.java
@@ -1,29 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.concurrent.RecursiveAction;
+
 public class MyFP {
 
     public static void main(String[] args) {
-       hello();
    //     hello("user");
 
-        double len =5;
-        System.out.println(area(len));
+        Square s = new Square(5); // создается оъект типа Square
+        System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a= 3;
-        double b =4;
-        System.out.println(area(a,b));
+
+        Rectangle r = new Rectangle(3, 4);
+        System.out.println("площадь прямуогольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
- public static void hello() {
-     System.out.println("Hello");
- }
 
 
-    public static double area( double l){
-        return l*l;
-    }
 
-    public static double area (double a,double b){
-        return a*b;
-    }
+
 }
